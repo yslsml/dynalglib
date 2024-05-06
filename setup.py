298265918d97179918508233135f365cb592dce5
Milana Antonova <mmf.antonovam@gmail.com>
2024-05-06 12:36:22 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from distutils.core import setup
 import pathlib
 
-VERSION = "0.0.4"
+VERSION = "1.3"
 
 setup(
     name="dynalglib",  # How you named your package folder (MyLib)
     packages=["dynalglib"],  # Chose the same as "name"
     version=VERSION,  # Start with a small number and increase it with every change you make
     license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-    description="Dynalglib is a library which is designed to solve some of dynamic programming algorithms in Python.",  # Give a short description about your library
+    description="Dynalglib is a library which is designed to solve problems using dynamic programming algorithms in Python.",  # Give a short description about your library
     long_description=pathlib.Path("README.md").read_text(),
     long_description_content_type="text/markdown",
     author="Milana Antonova",  # Type in your name
     author_email="mmf.antonovam@gmail.com",  # Type in your E-Mail
     url="https://github.com/yslsml/dynalglib",  # Provide either the link to your github or to your website
     project_urls={
-        # "Documentation":"",
-        "source": "https://github.com/yslsml/dynalglib"
+        "Documentation": "https://github.com/yslsml/dynalglib/wiki/Documentation",
+        "Source": "https://github.com/yslsml/dynalglib",
     },
     keywords=[
         "math",
@@ -25,6 +25,12 @@ setup(
         "algorithm",
         "dynamic programming",
         "knapsack problem",
+        "knapsack",
+        "traveling salesman problem",
+        "tsp",
+        "resource allocation",
+        "fibonacci",
+        "coin change",
     ],  # Keywords that define your package best
     classifiers=[
         "Development Status :: 3 - Alpha",  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
